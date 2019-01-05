46d6a56ed3544a44957928a38b09db43846b8da8
Rowan <rowanho45@gmail.com>
2019-01-05 12:00:02 +0000

    paypal payment for booking

---
+++ SE_nodejs/paypalApiFunctions.js
@@ -0,0 +1,106 @@
+const request = require('request');
+//gets an access token we can use for the paypal api
+const PAYPAL_URI = "https://api.sandbox.paypal.com";
+function createToken(clientId, secret,callback){
+	request.post({
+    	uri: PAYPAL_URI + "/v1/oauth2/token",
+    	headers: {
+        	"Accept": "application/json",
+        	"Accept-Language": "en_US",
+        	"content-type": "application/x-www-form-urlencoded"
+    	},
+    auth: {
+    'user': clientId,
+    'pass': secret
+    // 'sendImmediately': false
+  	},
+  	form: {
+    	"grant_type": "client_credentials"
+  	}
+	},function(error, response, body) {
+		if(error){
+			callbacl(error);
+		}
+		else{
+    		callback(false,JSON.parse(body).access_token);
+    	}
+	});
+}
+//creates a paypal payment by making a request to the api
+function createPayment(clientId,secret,price,redirectUri,cancelUri,callback){
+	request.post({
+		uri:PAYPAL_URI + "/v1/payments/payment",
+		headers:{
+        	"Accept": "application/json",
+        	"Accept-Language": "en_US"
+		},
+		auth:{
+			'user': clientId,
+			'pass': secret
+		},
+		body:{
+			intent:'sale',
+			payer:{
+				payment_method:'paypal'
+			},
+			transactions: [
+			{
+				amount: 
+				{
+					total:price,
+					currency:'GBP'
+				}
+			}],
+			redirect_urls:
+			{
+				return_url: redirectUri,
+				cancel_url: cancelUri
+			}
+		},
+		json: true
+	},function(err,response)
+	{
+		if(err){
+			callback(err);
+		}
+		else{
+			callback(false,response);
+		}
+	});
+}
+
+//executes the payment with a request to the paypal api
+function executePayment(clientId,secret,price,paymentId,payerId,callback){
+    request.post(PAYPAL_URI + '/v1/payments/payment/' + paymentId + '/execute',
+      {
+        auth:
+        {
+          user: clientId,
+          pass: secret
+        },
+        body:
+        {
+          payer_id: payerId,
+          transactions: [
+          {
+            amount:
+            {
+              total: price,
+              currency: 'GBP'
+            }
+          }]
+        },
+        json: true
+      },
+      function(err,response){
+      	if(err){
+      		callback(err)
+      	}
+      	else{
+      		callback(false,response)
+      	}
+      });
+}
+
+module.exports.createPayment = createPayment;
+module.exports.executePayment = executePayment;
