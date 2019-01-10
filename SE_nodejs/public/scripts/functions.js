8680e43f19bf1631a4d7c8e06e8d2ac75504b2eb
nikeshpatel99 <44257354+nikeshpatel99@users.noreply.github.com>
2019-01-10 21:35:40 +0000

    fixined functions.js conflict

---
+++ SE_nodejs/public/scripts/functions.js
@@ -0,0 +1,8 @@
+//can change this to a button listener if you want
+function toggleVisability(id){
+	var doc = document.getElementById(id);
+       if(doc.style.display == 'block')
+          doc.style.display = 'none';
+       else
+          doc.style.display = 'block';
+};
