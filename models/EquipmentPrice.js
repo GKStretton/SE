4892c781e20a5a27a44dcde3e7c2fddfab72c637
Greg Stretton <gkstretton@gmail.com>
2019-02-02 18:39:29 +0000

    Fix EquipmentPrice types

--- models/EquipmentPrice.js
+++ models/EquipmentPrice.js
@@ -4,9 +4,8 @@ var Types = keystone.Field.Types;
 var EquipmentPrice= new keystone.List('EquipmentPrice');
 
 EquipmentPrice.add({
-	description: {type: String, required:true, initial:true },
-	junior: { type: String, initial:true, required:true},
-	senior: { type: String, initial:true, required:true},
+	item: {type: String, required:true, initial:true },
+	price: { type: String, initial:true, required:true},
 });
 
 EquipmentPrice.schema.virtual('canAccessKeystone').get(function () {
