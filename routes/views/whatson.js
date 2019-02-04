4d70caeb834b1176926acba1c989688aa2387b8f
nikeshpatel99 <44257354+nikeshpatel99@users.noreply.github.com>
2019-02-04 02:15:19 +0000

    WIP whats on content loading

--- routes/views/whatson.js
+++ routes/views/whatson.js
@@ -5,6 +5,5 @@ module.exports = function (req, res) {
 	var locals = res.locals;
 	view.query("adult", keystone.list("AdultWhatsOn").model.find());
 	view.query("child", keystone.list("ChildWhatsOn").model.find());
-
 	view.render("whats-on");
 }
