4218f0056a7c5587ea66419fa686eb84effa1b51
nikeshpatel99 <44257354+nikeshpatel99@users.noreply.github.com>
2019-03-08 20:42:32 +0000

    Start timepicker change, force enquiry form on no availability or options

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -7,6 +7,16 @@ var availabilitySaturdayEnd;
 var availabilitySundayStart;
 var availabilitySundayEnd;
 var availability;
+var automated;
+var noOptions;
+
+function setAutomated(auto){
+	automated = auto;
+};
+
+function setOptions(opt){
+	options = !opt;
+};
 
 function setAvailability(hasAvailability,weekdayStart,weekdayEnd,saturdayStart,saturdayEnd,sundayStart,sundayEnd){
 	if(hasAvailability){
@@ -20,7 +30,43 @@ function setAvailability(hasAvailability,weekdayStart,weekdayEnd,saturdayStart,s
 	availability = hasAvailability;
 };
 
+function addHour(time) {
+	currentTime = parseInt(time.charAt(0) + time.charAt(1));
+	newTime = (currentTime+1)%24;
+	return newTime.toString();
+};
 
+function getRange(start,end,back){
+	//strip minutes
+	if(back){
+		start = addHour(start);
+		end = addHour(end);
+	}
+	start = start.charAt(0) + start.charAt(1) + ":00";
+	end = end.charAt(0) + end.charAt(1) + ":00";
+	let options = [];
+	currentOption = start;
+	while(currentOption !== end){
+		options.push(currentOption);
+		currentOption = addHour(currentOption) + ":00";
+	}
+	return options;
+};
+
+function getWeekdayRange(callback,back){
+	options = getRange(availabilityWeekdayStart,availabilityWeekdayEnd,back);
+	callback(options);
+};
+
+function getSaturdayRange(callback,back){
+	options = getRange(availabilitySaturdayStart,availabilitySaturdayEnd,back);
+	callback(options);
+};
+
+function getSundayRange(callback,back){
+	options = getRange(availabilitySundayStart,availabilitySundayEnd,back);
+	callback(options);
+};
 
 //Automated booking submit
 $(document).on("click","#automatedFormSubmit",function(){
@@ -75,6 +121,7 @@ $(document).on("click","#manualFormSubmit",function(){
 
 function getPrice(){
 	let formData = $("#facility-input-id").serialize() + '&' + $("#time-from-input").serialize() + '&' + $("#time-to-input").serialize();
+	console.log(formData);
 	$.ajax({
 		url:"/booking/price",
 		type:"GET",
@@ -90,13 +137,43 @@ function getPrice(){
 			$('#errMsg').text(error.responseText);
 		}
 	});
-}
+};
 
-function addHour(time) {
-	currentTime = parseInt(time.charAt(0) + time.charAt(1));
-	newTime = (currentTime+1)%24;
-	return newTime.toString();
-}
+function setWeekday(id,back){
+	$(id).empty();
+	getWeekdayRange(function(options){
+		for(var i=0;i<options.length;i++){
+			$(id).append( $("<option>")
+				.val(options[i])
+				.html(options[i])
+			);
+		};
+	},back);
+};
+
+function setSaturday(id,back){
+	$(id).empty();
+	getSatudayRange(function(options){
+		for(var i=0;i<options.length;i++){
+			$(id).append( $("<option>")
+				.val(options[i])
+				.html(options[i])
+			);
+		};
+	},back);
+};
+
+function setSunday(id,back){
+	$(id).empty();
+	getSundayRange(function(options){
+		for(var i=0;i<options.length;i++){
+			$(id).append( $("<option>")
+				.val(options[i])
+				.html(options[i])
+			);
+		};
+	},back);
+};
 
 function updateTimeRange(day){
 	//reset times to avoid conficts
@@ -105,100 +182,66 @@ function updateTimeRange(day){
 	switch(day){
 		//sunday
 		case 0:
-			//From time picker
-			fp = document.querySelector("#time-from-input")._flatpickr;
-			fp.set("minDate",availabilitySundayStart);
-			fp.set("maxDate",availabilitySundayEnd);
-			fp.setDate(availabilitySundayStart);
-			//To time picker
-			fp = document.querySelector("#time-to-input")._flatpickr;
-			fp.set("minDate",availabilitySundayStart);
-			fp.set("maxDate",availabilitySundayEnd);
-			fp.setDate(addHour(availabilitySundayStart));
+			if(automated){
+				setSunday("#time-from-input",false);
+				setSunday("#time-to-input",true);
+			}
+			
 			//Facility schedule - ORDER IS IMPORTANT
 			$("#calendar").fullCalendar("option","maxTime",availabilitySundayEnd);
 			$("#calendar").fullCalendar("option","minTime",availabilitySundayStart);
 			break;
 		//saturday
 		case 6:
-			//From time picker
-			fp = document.querySelector("#time-from-input")._flatpickr;
-			fp.set("minDate",availabilitySaturdayStart);
-			fp.set("maxDate",availabilitySaturdayEnd);
-			fp.setDate(availabilitySaturdayStart);
-			//To time picker
-			fp = document.querySelector("#time-to-input")._flatpickr;
-			fp.set("minDate",availabilitySaturdayStart);
-			fp.set("maxDate",availabilitySaturdayEnd);
-			fp.setDate(addHour(availabilitySaturdayStart));
+			if(automated){
+				setSaturday("#time-from-input",false);
+				setSaturday("#time-to-input",true);
+			}
+			
 			//Facility schedule - ORDER IS IMPORTANT
 			$("#calendar").fullCalendar("option","maxTime",availabilitySaturdayEnd);
 			$("#calendar").fullCalendar("option","minTime",availabilitySaturdayStart);
 			break;
 		//weekdays
 		default:
-			//From time picker
-			fp = document.querySelector("#time-from-input")._flatpickr;
-			fp.set("minDate",availabilityWeekdayStart);
-			fp.set("maxDate",availabilityWeekdayEnd);
-			fp.setDate(availabilityWeekdayStart);
-			//To time picker
-			fp = document.querySelector("#time-to-input")._flatpickr;
-			fp.set("minDate",availabilityWeekdayStart);
-			fp.set("maxDate",availabilityWeekdayEnd);
-			fp.setDate(addHour(availabilityWeekdayStart));
+			if(automated){
+				setWeekday("#time-from-input",false);
+				setWeekday("#time-to-input",true);
+			}
 			//Facility schedule - ORDER IS IMPORTANT
 			$("#calendar").fullCalendar("option","maxTime",availabilityWeekdayEnd);
 			$("#calendar").fullCalendar("option","minTime",availabilityWeekdayStart);
 	}
 };
 
-function getTimeFromConfig(){
-	return {
-		enableTime: true,
-		noCalendar: true,
-		dateFormat: "H:i",
-		time_24hr: true,
-		defaultDate: "00:00",
-		onChange(selectedDates, dateStr, instance){
-			//stops user entering non hour time and times that have start time past the end time
-			let timeFromInput = document.querySelector("#time-from-input")._flatpickr;
-			let timeToInput = document.querySelector("#time-to-input")._flatpickr;
-			let fromDate = new Date(selectedDates[0]);
-			let toDate = new Date(timeToInput.selectedDates[0]);
-			if (fromDate >= toDate){
-				fromDate.setHours(toDate.getHours() - 1);
-				fromDate.setMinutes(fromDate.getMinutes() + 30);
-				fromDate.setMinutes(0);
-				timeFromInput.setDate(fromDate);
-			}
-			else{
-				fromDate.setMinutes(fromDate.getMinutes() + 30);
-				fromDate.setMinutes(0);
-				console.log(fromDate)
-				timeFromInput.setDate(fromDate);
-			}
+$("#time-from-input").change(function() {
+	console.log("changing from");
+	let from = this.selectedIndex;
+	let to = $("#time-to-input").prop("selectedIndex");
+	if(from > to){
+		if((to-1) < 0){
+			this.prop("selectedIndex",0);
+			$("#time-to-input").prop("selectedIndex",1)
+		}else{
+			this.prop("selectedIndex",to-1);
 		}
-	};
-};
+	}
+});
 
-function getTimeToConfig(){
-	return {
-		enableTime: true,
-		noCalendar: true,
-		time_24hr: true,
-		dateFormat: "H:i",
-		defaultDate: "01:00",
-		onChange(selectedDates, dateStr, instance){
-			//stops user entering non hour time and times that have start time past the end time
-			let timeToInput = document.querySelector("#time-to-input")._flatpickr;
-			let fromDate = new Date(selectedDates[0]);
-			fromDate.setMinutes(fromDate.getMinutes() + 30);
-			fromDate.setMinutes(0);
-			timeToInput.setDate(fromDate);
+$("#time-to-input").change(function() {
+	console.log("changing from");
+	let to = this.selectedIndex;
+	let from = $("#time-from-input").prop("selectedIndex");
+	let len = $('#time-from-input > option').length;
+	if(to < from){
+		if(to+1 > length-1){
+			this.prop("selectedIndex",len-1);
+			$("#time-from-input").prop("selectedIndex",len-2)
+		}else{
+			this.prop("selectedIndex",from+1);
 		}
-	};
-};
+	}
+});
 
 function getCalendarConfig(){
 	return {
@@ -239,17 +282,14 @@ function getConfig(){
 
 $(document).ready(function(){
 	// Date picker
-	if(availability){
+	if(availability && noOptions){
 		$("#calendar").fullCalendar(getCalendarConfig());
 		flatpickr("#date-input",getConfig());
-		flatpickr("#time-from-input",getTimeFromConfig());
-		flatpickr("#time-to-input",getTimeToConfig());
-		updateTimeRange(new Date().getDay());
+		updateTimeRange(new Date().getDay())
 		$("#book-form").on("shown.bs.collapse", function () {
 			$("#calendar").fullCalendar('rerenderEvents');
 		});
 	}
-	getPrice();
 });
 
 //get pricing data, render on front end
@@ -257,13 +297,20 @@ $(document).on("change","#automatedForm",function(){
 	getPrice();
 });
 
+$(document).on("click", "#btn-book-form", function(){
+	getPrice();
+});
+
 $(document).on("click", ".fc-button", function(){
 	let date = $("#calendar").fullCalendar("getDate").toDate();
-	let picker = document.querySelector("#date-input")._flatpickr;
-	console.log(picker)
-	console.log(date)
-	picker.setDate(date);
-	updateTimeRange(date.getDay());
+	if(automated){
+		let picker = document.querySelector("#date-input")._flatpickr;
+		picker.setDate(date);
+	}
+	if(noOptions){
+		updateTimeRange(date.getDay());
+	}
+	
 });
 
 //Booking form show button
