9f5f932790c35cfeb26fc02f2a80fa85dd1a2726
nikeshpatel99 <44257354+nikeshpatel99@users.noreply.github.com>
2019-03-12 10:06:02 +0000

    Fix image link dependecies broken by upload refactor

--- routes/views/event.js
+++ routes/views/event.js
@@ -4,6 +4,6 @@ module.exports = (req, res) => {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	view.query("event", keystone.list("Activity").model.findOne({name:req.params.name}).populate("galleryImages topImage"));
+	view.query("event", keystone.list("Activity").model.findOne({_id:req.params.id}).populate("galleryImages topImage"));
 	view.render("event/events");
 }
